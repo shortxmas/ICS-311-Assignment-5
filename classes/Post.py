cca37aeb7e41e1d1231f18a6c37b4697a1452469
peachcrumb <pchang6@hawaii.edu>
2024-08-05 20:56:59 -0700

    Update Post.py

--- classes/Post.py
+++ classes/Post.py
@@ -1,6 +1,15 @@
 class Post:
-    def __init__(self, comments, content, viewed_by, created):
+    def __init__(self, owner, comments, content, viewed_by, created):
+        self.owner = owner
         self.comments = comments[]
         self.content = content
-        self.viewed_by = viewed_by[]
+        self.views = views[]
         self.created = created
+
+    def add_comment(self,text,author):
+        yap = {"quote":text,"yapper":author}
+        self.comments.append(yap)
+    
+    def add_view(self,viewer,view_date,view_time):
+        view = {"seen_by":viewer,"date":view_date,"time":view_time}
+        self.views.append(view)
