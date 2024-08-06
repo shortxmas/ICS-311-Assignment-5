ca628a23f75f4c5d50eb6827eb3b5919c6fdc411
peachcrumb <pchang6@hawaii.edu>
2024-08-05 23:17:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ from classes.User import User
 
 
 def task4main():
-# word cloud of interesting posts    
-    
+# word cloud of interesting posts   
 
+    person1 = User(username="bob12",birth_year=1988,zipcode=55378)
+    person2 = User(username="its_meg",birth_year=1996,real_name="megan ashby",gender="she/her",zipcode=90039)
+    person3 = User(username="payuspaytas",birth_year=1988,gender="f",zipcode=61032)
+    users = [person1,person2,person3]
+    person4 = User(username="muskrat",birth_year=1971,real_name="elon musn't",zipcode=17739)
+    users.append(person4)
+
+    postA = Post(owner="payuspaytas")
+    postB =
+    postC = 
+    postD = 
+    posts = [postA, postB, postC, postD]
 
 def main():
     task4main()
