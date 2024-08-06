4152f49fded84d601f6611492f50ac4ddc4228ca
peachcrumb <pchang6@hawaii.edu>
2024-08-05 23:35:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from classes.Post import Post
 from classes.User import User
+import datetime
 
 
 def task4main():
@@ -12,7 +13,7 @@ def task4main():
     person4 = User(username="muskrat",birth_year=1971,real_name="elon musn't",zipcode=17739)
     users.append(person4)
 
-    postA = Post(owner="payuspaytas")
+    postA = Post(owner="payuspaytas",content="like do dogs have brains??? serious question",created=datetime.datetime(2013,7,3,16,19,0,0,None,0))
     postB =
     postC = 
     postD = 
