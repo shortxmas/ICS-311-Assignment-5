752be605a05bf0f8f5c896b23df3ee533db48c87
peachcrumb <pchang6@hawaii.edu>
2024-08-05 22:56:51 -0700

    Update User.py

--- classes/User.py
+++ classes/User.py
@@ -1,13 +1,13 @@
 from classes.Post import Post
 class User:
-    def __init__(self,username,posts,connections,birth_year,real_name,gender,city,commented,viewed):
+    def __init__(self,username,posts,connections,birth_year,real_name,gender,zipcode,commented,viewed):
         self.username = username
         self.posts = posts[]
         self.connections = connections[]
         self.birth_year = birth_year
         self.real_name = real_name
         self.gender = gender
-        self.city = city
+        self.zipcode = zipcode
         self.commented = commented[]
         self.viewed = viewed[]
 
