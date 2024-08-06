81bda241442ba8ac45503bd521d414ced9b5d934
peachcrumb <pchang6@hawaii.edu>
2024-08-06 01:24:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from classes.Post import Post
 from classes.User import User
 import datetime
+import random
 
 
 def task4main():
@@ -26,11 +27,21 @@ def task4main():
     postC = Post(owner="bob12",content="Hi Reddit, what breed of dog should I get my 4 year old niece? I was thinking a golden retriever, alaskan malamute, or a poodle. Not looking for an older or half-grown dog, I want a puppy to be able to grow with her.",created=datetime.datetime(2023,9,30,1,49,0,0,None,0))
     person2.add_post(postC)   
     postD = Post(owner="its_meg",content="hey guys look at my new pup! i think i'll name it michelle obama <3",created=datetime.datetime.now())
-    person1.add_post(postD)       
-    posts = [postA, postB, postC, postD]
+    person1.add_post(postD)
+    postE = Post(owner="bob12",content="Kathy, if you see this message me. Your cat escaped the yard and destroyed my plants again.",created=datetime.datetime.now())
+    person1.add_post(postE)    
+    posts = [postA, postB, postC, postD, postE]
 
 # populate users' posts with comments and views
-
+    nonsense = "When using AI to write content as a helping functionality, the ethical dimension involved tends to be utilitarian lens because the writer’s quality of life may increase since using AI can save time that could be used for activities that positively impact one’s life."
+    for x in posts:
+        for y in users:
+            for z in users[y].connections:
+                if users[y].connections[z].type != "blocked":
+                    posts[x].add_view(users[y].username,date.datetime.now())
+                    posts[x].add_comment(random.choices(nonsense,k=150),users[y].username)
+                    users[y].add_history(posts[x])
+                    
 
 def main():
     task4main()
