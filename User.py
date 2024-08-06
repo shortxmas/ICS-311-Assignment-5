8965340a48adafa259c8a0666acef36bef7d1dc6
peachcrumb <pchang6@hawaii.edu>
2024-08-05 19:46:28 -0700

    Create User.py

---
+++ User.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self,username,posts,connections,birth_year,real_name,gender,city):
+        self.username = username
+        self.posts = posts[]
+        self.connections = connections[]
+        self.birth_year = birth_year
+        self.real_name = real_name
+        self.gender = gender
+        self.city = city
