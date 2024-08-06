12b4fe0ef6f1d03eb6a8753b8c15c46a3e4760c9
peachcrumb <pchang6@hawaii.edu>
2024-08-05 20:41:49 -0700

    Update User.py

--- classes/User.py
+++ classes/User.py
@@ -7,3 +7,9 @@ class User:
         self.real_name = real_name
         self.gender = gender
         self.city = city
+
+    def add_connection(self,other_user,conxn_type):
+        connection = {"user":other_user,"type":conxn_type}
+        self.connections.append(connection)
+        
+        
