94ed162e57872622d1c92f151a8af8f8725c0143
peachcrumb <pchang6@hawaii.edu>
2024-08-05 21:09:16 -0700

    Update Post.py

--- classes/Post.py
+++ classes/Post.py
@@ -9,9 +9,7 @@ class Post:
         self.created = created
 
     def add_comment(self,text,author):
-        yap = {"quote":text,"yapper":author}
-        self.comments.append(yap)
+        self.comments.append({"quote":text,"yapper":author})
     
     def add_view(self,viewer,view_date,view_time):
-        view = {"seen_by":viewer,"date":view_date,"time":view_time}
-        self.views.append(view)
+        self.views.append({"seen_by":viewer,"date":view_date,"time":view_time})
