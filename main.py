52e50cdab56d79daef30bae97190509c6768103e
peachcrumb <pchang6@hawaii.edu>
2024-08-05 23:45:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def task4main():
     users.append(person4)
 
     postA = Post(owner="payuspaytas",content="like do dogs have brains??? serious question",created=datetime.datetime(2013,7,3,16,19,0,0,None,0))
-    postB =
-    postC = 
-    postD = 
+    postB = Post(owner="muskrat",content="Always think about this with my little fluffy dog. My big dog does look like a wolf.",created=datetime.datetime(2024,5,4,9,7,0,0,None,0))
+    postC = Post(owner="bob12",content="Hi Reddit, what breed of dog should I get my 4 year old niece? I was thinking a golden retriever, alaskan malamute, or a poodle. Not looking for an older or half-grown dog, I want a puppy to be able to grow with her.",created=datetime.datetime(2023,9,30,1,49,0,0,None,0))
+    postD = Post(owner="its_meg",content="hey guys look at my new pup! i think i'll name it michelle obama <3",created=datetime.datetime.now())
     posts = [postA, postB, postC, postD]
 
 def main():
