0432856fa05397251c2badd453de83a889fdfb96
peachcrumb <pchang6@hawaii.edu>
2024-08-05 20:00:32 -0700

    Create Post.py

---
+++ classes/Post.py
@@ -0,0 +1,6 @@
+class Post:
+    def __init__(self, comments, content, viewed_by, created):
+        self.comments = comments[]
+        self.content = content
+        self.viewed_by = viewed_by[]
+        self.created = created
